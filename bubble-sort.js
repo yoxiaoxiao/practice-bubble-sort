54cb992712ce5c846e4665cfa4c5c06eae7e5bb9
yoxiaoxiao <xiaoxiaofu2866@gmail.com>
2024-09-21 11:14:54 -0700

    finished

--- bubble-sort.js
+++ bubble-sort.js
@@ -1,18 +1,24 @@
 
 function bubbleSort(arr) {
-
+  let swapped = true;
+  while(swapped){
+    swapped = false;
     // Iterate through the array
-
-      // If the current value is greater than its neighbor to the right
-        // Swap those values
-
-        // Do not move this console.log
+    for (let i = 0; i < arr.length - 1; i++) {
+    // If the current value is greater than its neighbor to the right
+      // Swap those values
+      if (arr[i] > arr[i + 1]) {
+        [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+        swapped = true;
         console.log(arr.join(","));
-
+      }
+    }
     // If you get to the end of the array and no swaps have occurred, return
-
     // Otherwise, repeat from the beginning
-
+  }
+  return arr;
 }
+arr = [2,4,6,8,1,3,5,7,9];
+console.log(bubbleSort(arr));
 
 module.exports = bubbleSort;
